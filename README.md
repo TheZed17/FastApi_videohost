4ce0f0360e2d863db700d50c087b2db4f32d2fe4
Igor Deminski <kartoschka17gmail.com>
2022-04-12 18:54:51 +0300

    Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple video hosting
+
+## Backend
+- Framework - FastAPI
+- ORM - ORMAR
+- DB - SQLite
+
+## About:
+- Authorization - oauth google
+- User upload short videos
+- View video
+- Moderation
+- Ratings
+- Subscribers
---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from db import database, metadata, engine
+
+from followers.api import follower_router
+from video.api import video_router
+from user.api import user_router
+
+
+app = FastAPI()
+
+metadata.create_all(engine)
+app.state.database = database
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    database_ = app.state.database
+    if not database_.is_connected:
+        await database_.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    database_ = app.state.database
+    if database_.is_connected:
+        await database_.disconnect()
+
+app.include_router(user_router)
+app.include_router(video_router)
+app.include_router(follower_router)
